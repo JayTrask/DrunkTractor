bc2c2349a826e04bf3000c50bc400a6742820aa8
JayTrask <jontrask@umass.edu>
2018-12-16 21:36:38 -0500

    Game thus far

---
+++ Mo' Wine/Assets/Scripts/FieldGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGenerator : MonoBehaviour {
+
+    GameObject[] tiles;
+    public GameObject GrassTile;
+    public GameObject WineTile;
+
+	// Use this for initialization
+	void Start () {
+        tiles = GameObject.FindGameObjectsWithTag("tile");
+        // print("tiles count:" + tiles.Length.ToString());
+        generateNewField();
+        // tileGetDebug();
+		
+	}
+	
+    void generateNewField()
+    {
+        int randomNumber = 0;
+        int RangeMax = 20;
+        int rangeGrass = 18;
+        for (int x = 0; x < tiles.Length; x++)
+        {
+            randomNumber = Random.Range(1, RangeMax);
+            if(randomNumber < rangeGrass)
+            {
+                //generate a grass tile
+                GameObject grass = Instantiate(GrassTile);
+                grass.transform.parent = tiles[x].transform;
+                grass.transform.Translate(tiles[x].GetComponent<Transform>().position);
+                grass.SetActive(true);
+            }
+            else
+            {
+                //generate wine tile
+                GameObject wine = Instantiate(WineTile);
+                wine.transform.parent = tiles[x].transform;
+                wine.transform.Translate(tiles[x].GetComponent<Transform>().position);
+                wine.SetActive(true);
+            }
+        }
+    }
+
+    void tileGetDebug()
+    {
+        for(int x = 0; x < tiles.Length; x++)
+        {
+            print("tile: " + x.ToString());
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Mo' Wine/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float moveSpeedHorizontal;
+    float moveSpeedVertical;
+    string moveDirection = "right";
+
+    public int framesBetweenMoves;
+    public int wineMeter = 3;
+
+    // timer is solely here for debugging purposes. Allows me to slow down the gameplay to observe interactions. Always leave at 0 unless debugging
+    // and control the speed of the tractor with the 'framesBetweenMoves' variable.
+    public int timer;
+    int localTimer;
+
+    int tileCount;
+
+    public GameObject tile1;
+    public GameObject tile2;
+    public GameObject tile3;
+    float leftRightSpace;
+    float upDownSpace;
+
+    // Use this for initialization
+    void Start()
+    {
+        leftRightSpace = Mathf.Abs(tile1.transform.position.x) - Mathf.Abs(tile2.transform.position.x);
+        upDownSpace = Mathf.Abs(tile1.transform.position.y) - Mathf.Abs(tile3.transform.position.y);
+
+        localTimer = timer;
+
+        tileCount = GameObject.FindGameObjectsWithTag("tile").Length;
+        print(tileCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveSpeedHorizontal = Input.GetAxisRaw("Horizontal");
+        moveSpeedVertical = Input.GetAxisRaw("Vertical");
+        if (moveSpeedVertical == 1) { moveDirection = "up"; }
+        if (moveSpeedVertical == -1) { moveDirection = "down"; }
+        if (moveSpeedHorizontal == 1) { moveDirection = "right"; }
+        if (moveSpeedHorizontal == -1) { moveDirection = "left"; }
+
+
+        if (localTimer == 0)
+        {
+            switch (moveDirection)
+            {
+                case "right":
+                    gameObject.transform.position = (new Vector3((gameObject.transform.position.x + leftRightSpace / framesBetweenMoves), gameObject.transform.position.y, gameObject.transform.position.z));
+                    break;
+
+                case "left":
+                    gameObject.transform.position = (new Vector3((gameObject.transform.position.x - leftRightSpace / framesBetweenMoves), gameObject.transform.position.y, gameObject.transform.position.z));
+                    break;
+
+                case "down":
+                    gameObject.transform.position = (new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y - leftRightSpace / framesBetweenMoves), gameObject.transform.position.z));
+                    break;
+
+                case "up":
+                    gameObject.transform.position = (new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + leftRightSpace / framesBetweenMoves), gameObject.transform.position.z));
+                    break;
+
+                default:
+                    print("Something is broken here hehe");
+                    break;
+            }
+            localTimer = timer;
+        }
+        else
+        {
+            localTimer = localTimer - 1;
+        }
+
+    }
+
+    private void consumeWine()
+    {
+        if (framesBetweenMoves - wineMeter > 0)
+        {
+            framesBetweenMoves -= wineMeter;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision");
+        if (collision.gameObject.tag == "wine")
+        {
+            collision.gameObject.SetActive(false);
+            consumeWine();
+            tileCount--;
+
+            if (tileCount == 0)
+            {
+                print("Game Over!");
+                gameObject.SetActive(false);//game over
+            }
+        }
+
+        if (collision.gameObject.tag == "grass")
+        {
+            collision.gameObject.SetActive(false);
+            tileCount--;
+
+            if (tileCount == 0)
+            {
+                print("Game over!");
+                gameObject.SetActive(false);//game over
+            }
+           
+        }
+    }
+}
